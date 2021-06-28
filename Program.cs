ee464696922cbef39a2eef88f8d23854e515cad8
dave <dave@syntaxflux.com>
2021-06-28 11:56:26 +0000

    expanded

--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace simple_parser
             Console.WriteLine("Hello World!");
 
             /*
-                exp := trm add
-                add := + trm mul | mul
-                mul := * trm
-                trm := num
-                num := [0-9](\.[0-9])?
+                exp    := trm addsub
+                addsub := (+|-) trm addsub | muldiv
+                muldiv := (*|/) trm muldiv | lambda
+                trm    := num
+                num    := [0-9](\.[0-9])?
             */
 
-            doLexer("2 * 3");
+            doLexer("3 + 2 * 3 + 1 / 2 - 0.5");
 
             Console.WriteLine(String.Join(", ", _lex));
 
@@ -43,34 +43,56 @@ namespace simple_parser
 
         static double p_exp(){
             var num = p_trm();
-            var res = p_add(num);
+            var res = p_addsub(num);
             return res;
         }
 
-        static double p_add(double num){
-            var op = peek();
+        static double p_addsub(double num){
+            double ret = num;
 
-            if(op == SYMB_ADD){
+            string op;
+            while((op = peek()) == SYMB_ADD || op == SYMB_SUB){
                 next();
 
                 var val = p_trm();
 
                 string p;
-                if((p = peek()) == null){
-                    return num += val;
+                if((p = peek()) != null){
+                    val = p_muldiv(val);
                 }
-                else {
-                    return num += p_mul(val);
-                }
-            }
 
-            return p_mul(num);
+                switch(op) {
+                    case SYMB_ADD: ret += val; break;
+                    case SYMB_SUB: ret -= val; break;
+                    default: throw new Exception();
+                };
+            }
+            
+            return p_muldiv(ret);
         }
     
-        static double p_mul(double num){
-            next();
+        static double p_muldiv(double num){
+            var ret = num;
 
-            return num * Double.Parse(next());
+            string op;
+            while((op = peek()) == SYMB_MUL || op == SYMB_DIV){
+                next();
+
+                var val = p_trm();
+
+                // string p;
+                // if((p = peek()) != null){
+                //     val += p_muldiv(num);
+                // }
+
+                switch(op) {
+                    case SYMB_MUL: ret *= val; break;
+                    case SYMB_DIV: ret /= val; break;
+                    default: throw new Exception();
+                };
+            }
+            
+            return ret;
         }
 
         static double p_trm(){
@@ -80,11 +102,11 @@ namespace simple_parser
         static double p_num() => Double.Parse(next());
         
 
-
         const string
             SYMB_ADD = "+",
             SYMB_SUB = "-",
-            SYMB_MUL = "*";
+            SYMB_MUL = "*",
+            SYMB_DIV = "/";
                 
         static string[] _symb = new [] { SYMB_ADD, SYMB_SUB, SYMB_MUL };
 
